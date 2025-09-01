f60ef2d2ec27e7350d30b65371ba7a37ae8404d8
eng-kerolos-adel <eng.kerolos.adel.eleshaa@gmail.com>
2025-09-01 17:48:31 +0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "v5jmypcwpk.ufs.sh",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
