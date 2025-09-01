b90100a7575ea84ba224b50ee05c85f9d31847c6
eng-kerolos-adel <eng.kerolos.adel.eleshaa@gmail.com>
2025-09-01 23:25:03 +0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -34,6 +35,7 @@ export default async function RootLayout({
         <Navbar session={session} />
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
